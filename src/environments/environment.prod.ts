73f9e1ea5a96b74b91c6cd408efdd3ea8e2a3423
Davide <davide.n2@gmx.de>
2023-06-01 11:46:55 +0200

    rework hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
 export const environment = {
     production: true,
     firebase: {
-      apiKey: "AIzaSyACKWugqCj3pOUR-N2Ggdc5JY4EyA8PV3E",
-      authDomain: "ring-of-fire-f4257.firebaseapp.com",
-      databaseURL: "https://ring-of-fire-f4257-default-rtdb.europe-west1.firebasedatabase.app",
-      projectId: "ring-of-fire-f4257",
-      storageBucket: "ring-of-fire-f4257.appspot.com",
-      messagingSenderId: "62591141712",
-      appId: "1:62591141712:web:9e6f26a89514b8802c4e55"
+      apiKey: "AIzaSyCDz9iTfPpRFXyaBWDL4km-x_hQX3B7bMc",
+      authDomain: "ringoffire-6553c.firebaseapp.com",
+      projectId: "ringoffire-6553c",
+      storageBucket: "ringoffire-6553c.appspot.com",
+      messagingSenderId: "957324350369",
+      appId: "1:957324350369:web:4d0c6d840027896bf431e0"
     },
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    projectId: 'ring-of-fire-f4257',
-    appId: '1:62591141712:web:9e6f26a89514b8802c4e55',
-    databaseURL: 'https://ring-of-fire-f4257-default-rtdb.europe-west1.firebasedatabase.app',
-    storageBucket: 'ring-of-fire-f4257.appspot.com',
+    projectId: 'ringoffire-6553c',
+    appId: '1:957324350369:web:4d0c6d840027896bf431e0',
+    storageBucket: 'ringoffire-6553c.appspot.com',
     locationId: 'europe-west',
-    apiKey: 'AIzaSyACKWugqCj3pOUR-N2Ggdc5JY4EyA8PV3E',
-    authDomain: 'ring-of-fire-f4257.firebaseapp.com',
-    messagingSenderId: '62591141712',
+    apiKey: 'AIzaSyCDz9iTfPpRFXyaBWDL4km-x_hQX3B7bMc',
+    authDomain: 'ringoffire-6553c.firebaseapp.com',
+    messagingSenderId: '957324350369',
   },
 };
